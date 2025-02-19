465813c6d83c70724445bfa3bb6f9f1cd0245e82
Linda Dang <lindadang801@gmail.com>
2025-02-20 10:55:46 +1100

    Initial Commit

---
+++ DogYearsCalculator/DogYearsCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogYearsCalculatorApp.swift
+//  DogYearsCalculator
+//
+//  Created by Linda Dang on 20/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct DogYearsCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
