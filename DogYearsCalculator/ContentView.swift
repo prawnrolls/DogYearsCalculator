48f5f2d928e896e94950e5b0bdeb583ae0c62b00
Linda Dang <lindadang801@gmail.com>
2025-02-20 11:42:17 +1100

    updated

--- DogYearsCalculator/ContentView.swift
+++ DogYearsCalculator/ContentView.swift
@@ -8,12 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    //write values outside the body, views inside the body
+    @State private var age = 0.0
+    
+    //declare a function that returns a double
+    func calculateDogYears() -> Double {
+        return age * 7
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+           
+            switch age {
+            case 0...15:
+                Text("🐶") //hold function to search emojis
+                    .font(.system(size: 50))
+            case 16...25:
+                Text("😇")
+                    .font(.system(size: 50))
+            default:
+                Text("🤔")
+                    .font(.system(size: 50))
+            }
+            Text("Dog Years Calculator")
+                .font(.largeTitle)
+                .fontDesign(.rounded)
+                .fontWeight(.black)
+            Slider(value: $age, in: 0...25, step: 1)
+            Text("My age: \(Int(age))")
+                .font(.title2)
+            Text("Dog's age: \(Int(calculateDogYears()))")
+                .font(.title2)
+
         }
         .padding()
     }
